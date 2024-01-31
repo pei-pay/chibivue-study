0664fead3f262159c39d32c0181e6d3afbc6cad0
jumpei yamada <jumpei_yamada@mates-system.com>
2024-01-31 19:13:48 +0900

    tweak: adding log

--- packages/compiler-core/compile.ts
+++ packages/compiler-core/compile.ts
@@ -4,6 +4,9 @@ import { baseParse } from "./parse";
 export function baseCompile(template: string) {
   const parseResult = baseParse(template.trim());
   console.log("🚀 ~ baseCompile ~ parseResult:", parseResult) // parse結果確認用
+
   const code = generate(parseResult);
+  console.log("🚀 ~ baseCompile ~ code:", code) // compile結果表示
+
   return code;
 }
