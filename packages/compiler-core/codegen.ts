39d7d442017f3b02d1d4a1c9bd1102bc38c75495
pei-pay <gleektennis@icloud.com>
2024-03-17 00:05:07 +0900

    tweak: sfc template compile

--- packages/compiler-core/codegen.ts
+++ packages/compiler-core/codegen.ts
@@ -1,4 +1,4 @@
-import { toHandlerKey } from "../shared/general";
+import { toHandlerKey } from '../shared';
 import {
   AttributeNode,
   DirectiveNode,
@@ -7,8 +7,8 @@ import {
   NodeTypes,
   TemplateChildNode,
   TextNode,
-} from "./ast";
-import { CompilerOptions } from "./options";
+} from './ast';
+import { CompilerOptions } from './options';
 
 export const generate = (
   {
@@ -19,46 +19,50 @@ export const generate = (
   option: Required<CompilerOptions>,
 ): string => {
   return `${option.isBrowser ? 'return ' : ''}function render(_ctx) {
-  const { h } = ChibiVue;
-  return ${genNode(children[0])};
+    ${option.isBrowser ? 'with (_ctx) {' : ''}
+      const { h } = ChibiVue;
+      return ${genNode(children[0], option)};
+    ${option.isBrowser ? '}' : ''}
 }`;
 };
 
-const genNode = (node: TemplateChildNode): string => {
+const genNode = (
+  node: TemplateChildNode,
+  option: Required<CompilerOptions>,
+): string => {
   switch (node.type) {
     case NodeTypes.ELEMENT:
-      return genElement(node);
+      return genElement(node, option);
     case NodeTypes.TEXT:
       return genText(node);
     case NodeTypes.INTERPOLATION:
-      return genInterpolation(node);
+      return genInterpolation(node, option);
     default:
-      return "";
+      return '';
   }
 };
 
-const genElement = (el: ElementNode): string => {
+const genElement = (
+  el: ElementNode,
+  option: Required<CompilerOptions>,
+): string => {
   return `h("${el.tag}", {${el.props
-    .map(prop => genProp(prop))
-    .join(", ")}}, [${el.children.map((it) => genNode(it)).join(", ")}])`;
+    .map(prop => genProp(prop, option))
+    .join(', ')}}, [${el.children.map(it => genNode(it, option)).join(', ')}])`;
 };
 
-const genText = (text: TextNode): string => {
-  return `\`${text.content}\``;
-};
-
-const genInterpolation = (node: InterpolationNode): string => {
-  return `${node.content}`;
-};
-
-const genProp = (prop: AttributeNode | DirectiveNode): string => {
+const genProp = (
+  prop: AttributeNode | DirectiveNode,
+  option: Required<CompilerOptions>,
+): string => {
   switch (prop.type) {
     case NodeTypes.ATTRIBUTE:
       return `${prop.name}: "${prop.value?.content}"`;
     case NodeTypes.DIRECTIVE: {
       switch (prop.name) {
         case 'on':
-          return `${toHandlerKey(prop.arg)}: ${prop.exp}`;
+          return `${toHandlerKey(prop.arg)}: ${option.isBrowser ? '' : '_ctx.'
+            }${prop.exp}`;
         default:
           // TODO: other directives
           throw new Error(`unexpected directive name. got "${prop.name}"`);
@@ -67,4 +71,15 @@ const genProp = (prop: AttributeNode | DirectiveNode): string => {
     default:
       throw new Error(`unexpected prop type.`);
   }
+};
+
+const genText = (text: TextNode): string => {
+  return `\`${text.content}\``;
+};
+
+const genInterpolation = (
+  node: InterpolationNode,
+  option: Required<CompilerOptions>,
+): string => {
+  return `${option.isBrowser ? '' : '_ctx.'}${node.content}`;
 };
