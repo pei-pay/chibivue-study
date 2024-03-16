fcec506afb375ca54c95e838ab1c2f1b3dcfbd88
pei-pay <gleektennis@icloud.com>
2024-03-17 00:32:19 +0900

    feat: sfc style

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import type { Plugin } from 'vite';
 import { createFilter } from 'vite';
 import { parse, rewriteDefault } from '../../compiler-sfc';
@@ -9,22 +10,37 @@ export default function vitePluginChibivue(): Plugin {
   return {
     name: 'vite:chibivue',
 
+    resolveId(id) {
+      // このidは実際には存在しないパスだが、loadで仮想的にハンドリングするのでidを返してあげる (読み込み可能だということにする)
+      if (id.match(/\.vue\.css$/)) return id;
+      // ここでreturnされないidに関しては、実際にそのファイルが存在していたらそのファイルが解決されるし、存在していなければ存在しないというエラーになる
+    },
+    load(id) {
+      // .vue.cssがloadされた (importが宣言され、読み込まれた) ときのハンドリング
+      if (id.match(/\.vue\.css$/)) {
+        const filename = id.replace(/\.css$/, '');
+        const content = fs.readFileSync(filename, 'utf-8');
+        const { descriptor } = parse(content, { filename });
+        const styles = descriptor.styles.map(it => it.content).join('\n');
+        return { code: styles };
+      }
+    },
+
     transform(code, id) {
       if (!filter(id)) return;
 
       const outputs = [];
       outputs.push("import * as ChibiVue from 'chibivue'");
+      outputs.push(`import '${id}.css'`); // ${id}.cssのimport文を宣言しておく
 
       const { descriptor } = parse(code, { filename: id });
 
-      // --------------------------- ここから
       const SFC_MAIN = '_sfc_main';
       const scriptCode = rewriteDefault(
         descriptor.script?.content ?? '',
         SFC_MAIN,
       );
       outputs.push(scriptCode);
-      // --------------------------- ここまで
 
       const templateCode = compile(descriptor.template?.content ?? '', {
         isBrowser: false,
@@ -32,7 +48,7 @@ export default function vitePluginChibivue(): Plugin {
       outputs.push(templateCode);
 
       outputs.push('\n');
-      outputs.push(`export default { ...${SFC_MAIN}, render }`); // ここ
+      outputs.push(`export default { ...${SFC_MAIN}, render }`);
 
       return { code: outputs.join('\n') };
     },
