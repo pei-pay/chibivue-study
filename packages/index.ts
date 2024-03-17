b9edca408c8df60c02df290287d9d91412587fef
pei-pay <gleektennis@icloud.com>
2024-03-17 13:10:02 +0900

    first commit

---
+++ packages/index.ts
@@ -0,0 +1,18 @@
+export type Options = {
+  render: () => string;
+};
+
+export type App = {
+  mount: (selector: string) => void;
+};
+
+export const createApp = (options: Options): App => {
+  return {
+    mount: selector => {
+      const root = document.querySelector(selector);
+      if (root) {
+        root.innerHTML = options.render();
+      }
+    }
+  };
+};
