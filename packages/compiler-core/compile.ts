a6b5aa069808b84dfe44a946def6e350706ab88d
pei-pay <gleektennis@icloud.com>
2024-07-21 20:32:50 +0900

    feat: ast compiler

--- packages/compiler-core/compile.ts
+++ packages/compiler-core/compile.ts
@@ -5,5 +5,6 @@ import { baseParse } from './parse'
 export function baseCompiler(template: string) {
   const parsedResult = baseParse(template)
   const code = generate(parsedResult)
+  console.log("🚀 ~ baseCompiler ~ code:", code)
   return code
 }
