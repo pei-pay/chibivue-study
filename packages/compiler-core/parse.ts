8c89a3c2bcf836e69120d6a3f49d9c2f39be8053
pei-pay <gleektennis@icloud.com>
2024-07-21 20:15:19 +0900

    feat: parseElement

--- packages/compiler-core/parse.ts
+++ packages/compiler-core/parse.ts
@@ -1,4 +1,5 @@
-import { ElementNode, NodeTypes, Position, TemplateChildNode, TextNode, SourceLocation } from "./ast";
+import { parse } from "path";
+import { ElementNode, NodeTypes, Position, TemplateChildNode, TextNode, SourceLocation, AttributeNode } from "./ast";
 
 
 export interface ParserContext {
@@ -26,8 +27,7 @@ export const baseParse = (
 ): { children: TemplateChildNode[]; } => {
   const context = createParserContext(content);
   const children = parseChildren(context, []); // 子ノードをパース
-  // TODO: 
-  return { children: [] };
+  return { children };
 };
 
 function parseChildren(
@@ -64,7 +64,7 @@ function isEnd(context: ParserContext, ancestors: ElementNode[]): boolean {
   // sが"</"で始まり、かつその後にancestorsのタグ名が続くことを判定し、閉じタグがあるか(parseChildrenが終了するべきか)を判定する
   if (startsWith(s, '<')) {
     for (let i = ancestors.length - 1; i >= 0; --i) {
-      if (starsWithEndTagOpen(s, ancestors[i].tag)) {
+      if (startsWithEndTagOpen(s, ancestors[i].tag)) {
         return true;
       }
     }
@@ -93,7 +93,7 @@ function last<T>(xs: T[]): T | undefined {
   return xs[xs.length - 1];
 }
 
-function starsWithEndTagOpen(source: string, tag: string): boolean {
+function startsWithEndTagOpen(source: string, tag: string): boolean {
   return (
     startsWith(source, '</') &&
     // source 文字列の3文字目（インデックス2）から、タグ名の長さ分の文字列を抽出し、それが tag と一致するか
@@ -135,6 +135,13 @@ function advanceBy(context: ParserContext, numberOfCharacters: number) {
   advancePositionWithMutation(context, source, numberOfCharacters);
   context.source = source.slice(numberOfCharacters);
 }
+function advanceSpaces(context: ParserContext): void {
+  const match = /^[\t\r\n\f ]+/.exec(context.source);
+  if (match) {
+    advanceBy(context, match[0].length);
+  }
+}
+
 
 function advancePositionWithMutation(
   pos: Position,
@@ -173,4 +180,157 @@ function getSelection(
     end,
     source: context.originalSource.slice(start.offset, end.offset)
   };
+}
+
+const enum TagType {
+  Start,
+  End
+}
+
+function parseElement(
+  context: ParserContext,
+  ancestors: ElementNode[]
+): ElementNode | undefined {
+  // Start tag
+  const element = parseTag(context, TagType.Start);
+
+  if (element.isSelfClosing) {
+    return element;
+  }
+
+  // Children
+  ancestors.push(element);
+  const children = parseChildren(context, ancestors);
+  ancestors.pop();
+
+  element.children = children;
+
+  // End tag
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End);
+  }
+
+  return element;
+}
+
+
+function parseTag(context: ParserContext, type: TagType): ElementNode {
+  const start = getCursor(context);
+  const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source)!;
+  const tag = match[1];
+
+  advanceBy(context, match[0].length);
+  advanceSpaces(context);
+
+  // Attributes
+  let props = parseAttributes(context, type);
+
+  // Tag Close
+  let isSelfClosing = false;
+
+  isSelfClosing = startsWith(context.source, '/>');
+  advanceBy(context, isSelfClosing ? 2 : 1);
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+    props,
+    children: [],
+    isSelfClosing,
+    loc: getSelection(context, start)
+  };
+}
+
+function parseAttributes(
+  context: ParserContext,
+  type: TagType
+): AttributeNode[] {
+  const props = [];
+  const attributeNames = new Set<string>();
+
+  while (
+    context.source.length > 0 &&
+    !startsWith(context.source, '>') &&
+    !startsWith(context.source, '/>')
+  ) {
+    const attr = parseAttribute(context, attributeNames);
+
+    if (type === TagType.Start) {
+      props.push(attr);
+    }
+
+    advanceSpaces(context); // スペースは読み飛ばす
+  }
+
+  return props;
+}
+
+type AttributeValue = {
+  content: string;
+  loc: SourceLocation;
+} | undefined;
+
+
+function parseAttribute(
+  context: ParserContext,
+  namesSet: Set<string>
+): AttributeNode {
+  // Name
+  const start = getCursor(context);
+  const match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source)!;
+  const name = match[0];
+
+  namesSet.add(name);
+
+  advanceBy(context, name.length);
+
+  let value: AttributeValue = undefined;
+
+  if (/^[\t\r\n\f ]*=/.test(context.source)) {
+    advanceSpaces(context);
+    advanceBy(context, 1);
+    advanceSpaces(context);
+    value = parseAttributeValue(context);
+  }
+
+  const loc = getSelection(context, start);
+
+  return {
+    type: NodeTypes.ATTRIBUTE,
+    name,
+    value: value && {
+      type: NodeTypes.TEXT,
+      content: value.content,
+      loc: value.loc
+    },
+    loc
+  };
+}
+
+function parseAttributeValue(context: ParserContext): AttributeValue {
+  const start = getCursor(context);
+  let content: string;
+
+  const quote = context.source[0];
+  const isQuoted = quote === `"` || quote === `'`;
+  if (isQuoted) {
+    advanceBy(context, 1);
+
+    const endIndex = context.source.indexOf(quote);
+    if (endIndex === -1) {
+      content = parseTextData(context, context.source.length);
+    } else {
+      content = parseTextData(context, endIndex);
+      advanceBy(context, 1);
+    }
+  } else {
+    // Unquoted
+    const match = /^[^\t\r\n\f >]+/.exec(context.source);
+    if (!match) {
+      return undefined;
+    }
+    content = parseTextData(context, match[0].length);
+  }
+
+  return { content, loc: getSelection(context, start) };
 }
