f7112730226ea06db3825bccb0f21e6cb097ecc1
pei-pay <gleektennis@icloud.com>
2024-07-21 17:24:08 +0900

    feat: wip ast parser

--- packages/compiler-core/parse.ts
+++ packages/compiler-core/parse.ts
@@ -1,17 +1,104 @@
+import { ElementNode, NodeTypes, TemplateChildNode } from "./ast";
+
+
+export interface ParserContext {
+  // 元々のテンプレート文字列
+  readonly originalSource: string
+  source: string
+  // このパーサが読み取っている現在地
+  offset: number
+  line: number
+  column: number
+}
+
+function createParserContext(content: string): ParserContext {
+  return {
+    originalSource: content,
+    source: content,
+    column: 1,
+    line: 1,
+    offset: 0
+  }
+}
+
 export const baseParse = (
   content: string
-): { tag: string, props: Record<string, string>, textContent: string; } => {
-  const matched = content.match(/<(\w+)\s+([^>]*)>([^<]*)<\/\1>/);
-  if (!matched) return { tag: '', props: {}, textContent: '' };
-
-  const [_, tag, attrs, textContent] = matched
-
-  const props: Record<string, string> = {}
-  attrs.replace(/(\w+)=["']([^"']*)["']/g, (_, key: string, value: string) => {
-    props[key] = value;
-    return '';
-  })
-
-
-  return { tag, props, textContent };
+): { children: TemplateChildNode[] } => {
+  const context = createParserContext(content)
+  const children = parseChildren(context, []) // 子ノードをパース
+  // TODO: 
+  return { children: [] }
 };
+
+function parseChildren(
+  context: ParserContext,
+  ancestors: ElementNode[]
+): TemplateChildNode[] {
+  const nodes: TemplateChildNode[] = []
+
+  while(!isEnd(context, ancestors)) {
+    const s = context.source
+    let node: TemplateChildNode | undefined = undefined
+
+    if(s[0] === '<') {
+      // sが"<"で始まり、かつ次の文字がアルファベットの場合は要素としてパース
+      if(/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
+    }
+
+    if(!node) {
+      node = parseText(context)
+    }
+
+    pushNode(nodes, node)
+  }
+
+  return nodes
+}
+
+// 子要素パースの while を判定(パース終了)するための関数
+function isEnd(context: ParserContext, ancestors: ElementNode[]): boolean {
+  const s = context.source
+
+  // sが"</"で始まり、かつその後にancestorsのタグ名が続くことを判定し、閉じタグがあるか(parseChildrenが終了するべきか)を判定する
+  if(startsWith(s, '<')) {
+    for(let i = ancestors.length -1; i >= 0; --i) {
+      if(starsWithEndTagOpen(s, ancestors[i].tag)) {
+        return true
+      }
+    }
+  }
+
+  return !s
+}
+
+function startsWith(source: string, searchString: string): boolean {
+  return source.startsWith(searchString)
+}
+
+function pushNode(nodes: TemplateChildNode[], node: TemplateChildNode): void {
+  if (node.type === NodeTypes.TEXT) {
+    const prev = last(nodes)
+    if(prev && prev.type === NodeTypes.TEXT) {
+      prev.content += node.content
+      return
+    }
+  }
+  nodes.push(node)
+}
+
+// 配列の最後の要素を抽出
+function last<T>(xs: T[]): T | undefined {
+  return xs[xs.length - 1]
+}
+
+function starsWithEndTagOpen(source: string, tag: string): boolean {
+  return (
+    startsWith(source, '</') &&
+    // source 文字列の3文字目（インデックス2）から、タグ名の長さ分の文字列を抽出し、それが tag と一致するか
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase() &&
+    // タグ名の直後の文字が、タブ、改行、フォームフィード、スペース、/ または > のいずれかであるか
+    /[\t\r\n\f />]/.test(source[2 + tag.length] || '>')
+  );
+}
