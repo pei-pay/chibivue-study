ba67696c8476e4498565f6f9e0e75585bbb62135
pei-pay <gleektennis@icloud.com>
2024-08-04 13:36:04 +0900

    feat: wip sfc

---
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -0,0 +1,13 @@
+import type { Plugin } from 'vite'
+import { createFilter } from 'vite'
+
+export default function vitePluginChibivue(): Plugin {
+  const filter = createFilter(/\.vue$/)
+  return {
+    name: 'vite:chibivue',
+    transform(code, id) {
+      if (!filter(id)) return
+      return { code: `export default {}` }
+    }
+  }
+}
